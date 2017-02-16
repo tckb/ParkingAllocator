15367e30a8812424f2448961c7a61c55a6a4fb46
tckb <tckb.504@gmail.com>
2017-02-16 19:34:19 +0530

    comiting the initial skelton of the program.

---
+++ src/main/java/com/gojek/pl/main/MainRunner.java
@@ -0,0 +1,12 @@
+package com.gojek.pl.main;
+
+/**
+ * Main entry point to the program
+ *
+ * @author tckb
+ */
+public class MainRunner {
+    public static void main(String[] args) {
+        // do magic here
+    }
+}
